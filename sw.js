d190ef7c1e83efa2dd17480482b57abe50777850
Denis Moskvin <denis.moskvin@austromyrtus.ru>
2026-02-09 16:50:49 +0300

    0.1.4

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 ﻿const CACHE_NAME = 'position-calc-v1';
 const urlsToCache = [
-    '/',
-    'index.html',
-    'manifest.json'
+    './',
+    './index.html',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
