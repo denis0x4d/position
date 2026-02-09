47a3abdf0a5fb6072d798c6a740cccf2bc225262
Denis Moskvin <denis.moskvin@austromyrtus.ru>
2026-02-09 16:46:50 +0300

    0.1.3

--- sw.js
+++ sw.js
@@ -12,9 +12,15 @@ self.addEventListener('install', event => {
     );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
     );
 });
+
+
